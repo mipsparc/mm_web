7a963e3258c0c7f03b02d64f5a2f34e23d4e87c6
mipsparc <mipsparc@gmail.com>
2021-06-30 22:13:19 +0900

    All rights reserved

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import version
 
 app = Flask(__name__)
 
+UPLOAD_FILENAME = '/tmp/new_firmware.tar.gz2'
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def menu():
+    return render_template('menu.html', version=version.VERSION, menu=True)
+
+@app.route("/upgrade", methods=['GET', 'POST'])
+def upgrade():
+    if request.method == 'GET':
+        return render_template('upgrade.html', version=version.VERSION)
+    elif request.method == 'POST':
+        f = request.files['file']
+        f.save('/var/www/uploads/uploaded_file.txt')

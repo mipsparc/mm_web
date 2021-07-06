490859c338303e075001d6615ac2a4eff0f6a8a3
mipsparc <mipsparc@pixiv.com>
2021-07-06 09:58:52 +0900

    wip

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, Response, flash, Markup
 import version
+import os
+import israspi
+import glob
+import datetime
+from subprocess import Popen
 
 app = Flask(__name__)
+# アプライアンスのため固定鍵とする。セキュリティは必要とされない。
+# 同様に、CSRFなども自由だが、現実的な攻撃を受けづらいので対策しない。
+app.secret_key = b'\xda\x90\x89lh\xdb\x80\xee"$\xa1#\xc4\'\xcc\xc6\xde|\x18~\xdc\x86\x14\xa6'
 
-UPLOAD_FILENAME = '/tmp/new_firmware.tar.gz2'
+if israspi.is_raspi:
+    FIRMWARE_FILENAME = '/mnt/rescue/new_firmware.tar.bz2'
+    DATABASE_FILENAME = '/mnt/database/multimascon.sqlite3'
+    LOGS = '/mnt/database/log/*.txt'
+else:
+    FIRMWARE_FILENAME = '/tmp/new_firmware.tar.bz2'
+    # ホームディレクトリにsymlinkを置く
+    from pathlib import Path
+    DATABASE_FILENAME = str(Path.home()) + '/multimascon.sqlite3'
+    LOGS = './MultiMascon/log/*.txt'
 
 @app.route("/")
 def menu():
@@ -15,4 +32,47 @@ def upgrade():
         return render_template('upgrade.html', version=version.VERSION)
     elif request.method == 'POST':
         f = request.files['file']
-        f.save('/var/www/uploads/uploaded_file.txt')
+        blob = f.read()
+        if len(blob) < 100:
+            return redirect(url_for('upgrade'))
+        
+        open(FIRMWARE_FILENAME, mode='wb').write(blob)
+        Popen('sleep 5; reboot', shell=True)
+        return redirect(url_for('upgrade_ready'))
+
+@app.route("/upgrade_ready")
+def upgrade_ready():
+    return render_template('upgrade_ready.html', version=version.VERSION)
+
+@app.route("/database", methods=['GET', 'POST'])
+def database():
+    if request.method == 'GET':
+        return render_template('database.html', version=version.VERSION)
+    elif request.method == 'POST':
+        f = request.files['file']
+        blob = f.read()
+        if len(blob) < 100:
+            return redirect(url_for('database'))
+        
+        open(DATABASE_FILENAME, mode='wb').write(blob)
+        flash('データベースの更新が完了しました。すぐに再起動を行ってください')
+        return redirect(url_for('database'))
+    
+@app.route("/database/multimascon.sqlite3")
+def database_download():
+    database_file = open(DATABASE_FILENAME, mode='rb').read()
+    return database_file, 200, {'Content-Type': 'application/octet-stream'}
+
+@app.route("/log")
+def log():
+    log = ''
+    
+    log_files = glob.glob(LOGS)
+    if len(log_files) == 0:
+        log = '見つかりませんでした'
+    else:
+        log_files.sort(reverse=True)
+        for filename in log_files:
+            log += open(filename).read().replace('\n', '<br>') + '<br>------------------------------------<br>'
+        
+    return render_template('log.html',  version=version.VERSION, log=Markup(log))
